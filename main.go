07ca90ff4f8a6f1136c6a058457ed92c896424e8
skill <skill@skilldeMacBook-Pro.local>
2019-07-03 17:33:05 +0800

    initial beans

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "beans/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"beans/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
