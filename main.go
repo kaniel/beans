79b1763a1acf01cf84bc80b3f8d4aaeb62bac255
kaniel <you@example.com>
2019-07-11 20:56:56 +0800

    initialize syncdb and database

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"beans/models"
 	_ "beans/routers"
+	"fmt"
 	"github.com/astaxie/beego"
+	"os"
 )
 
+const VERSION = "0.1.1"
+
 func main() {
+	//initialize()
 	beego.Run()
 }
 
+func init() {
+	fmt.Println(beego.AppConfig.String("database::db_name"))
+	initialize()
+}
+//func Run()  {
+//	initialize()
+//}
+
+func initialize() {
+
+	initArgs()
+
+	models.Connect()
+}
+
+func initArgs() {
+	args := os.Args
+	for _, v := range args {
+		if v == "-syncdb" {
+			models.Syncdb()
+			os.Exit(0)
+		}
+	}
+}
--- models/users.go
+++ models/users.go
@@ -1,63 +1,202 @@
 package models
 
 import (
+	"fmt"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 	"time"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var o orm.Ormer
+
+
 type User struct {
-	Id 			int			`orm:"auto;PK"`
+	Id 			int64		`orm:"auto;PK"`
 	email 		string
 	Uid 		string
 	Pword		string
-	Active		int			`orm:"default:0"`  //0:正常，1:禁用
-	UserInfo	*UserInfo 	`orm:"rel(one)"`
-	UserLog		*UserLog 	`orm:"reverse(many)"`
+	Active		int			`orm:"default(0)"`	  //0:正常，1:禁用
+	Profile		*Profile 	`orm:"rel(one)"`
+	Role		*Role	 	`orm:"rel(one)"`
+	UsersLog	[]*UsersLog `orm:"reverse(many)"`
 	CreateTime	time.Time	`orm:"auto_now_add;type(datetime)`
 }
 
 type Role struct {
-	Id 			int			`orm:"auto;PK"`
+	Id 			int64		`orm:"auto;PK"`
 	Name		string
-	Mark		int			`orm:"default:0"`  // 0:普通用户,1:管理员,2:超级管理员
-	Description string
+	Status		int			`orm:"default(0)"` 	 // 0:普通用户,1:管理员,2:超级管理员
+	Description string		`orm:"default(null)"`
 }
 
-type UserInfo struct {
-	Id 			int			`orm:"auto;PK"`
-	UserId 		int
-	Nickname	string
-	Age			int			`orm:"default:0"`  // 0 is nil
-	sex 		int 		`orm:"default:0"`  //male:1, female:2
-	Phone 		string
-	WeixinId	string
-	Beans		int			`orm:"default:0"`
-	Icon		string
-	Photo		string
-	LoginCount	int			`orm:"default:0"`
-	UpdateTime	time.Time	`orm:"auto_now_add;type(datetime)`
+type Profile struct {
+	Id 			int64		`orm:"auto;PK"`
+	User 		*User		`orm:"reverse(one)"`
+	Nickname	string		`orm:"default(null)"`
+	Age			int			`orm:"default(0)"`   // 0 is nil
+	Sex 		int 		`orm:"default(0)"`   //male:1, female:2
+	Phone 		string		`orm:"default(null)"`
+	WeixinId	string		`orm:"default(null)"`
+	Beans		int			`orm:"default(0)"`
+	Icon		string		`orm:"default(null)"`
+	Photo		string		`orm:"default(null)"`
+	LoginCount	int			`orm:"default(0)"`
+	UpdateTime	time.Time	`orm:"auto_now;type(datetime)`
 }
 
-type UserLog struct {
-	Id 			int			`orm:"auto;PK"`
-	loginIp		string
-	loginTime	time.Time	`orm:"auto_now_add;type(datetime)`
-	device		string
+type UsersLog struct {
+	Id 			int64		`orm:"auto;PK"`
+	User 		*User		`orm:"rel(fk)"`
+	LoginIp		string
+	LoginTime	time.Time	`orm:"auto_now_add;type(datetime)`
+	Device		string		`orm:"default(null)"`
 }
 
 type RushOrder struct {
-	Id 			int			`orm:"auto;PK"`
+	Id 			int64		`orm:"auto;PK"`
 	RushId		string
-	UserId		int
+	UserId		int64
 	UserUid		string
-	RushNum		int			`orm:"default:0"`
+	RushNum		int
 	CreateTime	time.Time	`orm:"auto_now_add;type(datetime)`
 }
 
 type ConstValue struct {
-	Id 			int			`orm:"auto;PK"`
+	Id 			int64		`orm:"auto;PK"`
 	ConstType	int
 	ValueStr	string
-	UseType		int			`orm:"default:0"`
-	UpdateTime	time.Time
+	UseType		int			`orm:"default(1)"`
+	UpdateTime	time.Time	`orm:"auto_now;type(datetime)`
 	CreateTime	time.Time	`orm:"auto_now_add;type(datetime)`
+}
+
+
+func init() {
+	orm.RegisterModel(new(User), new(Profile),
+		new(Role), new(ConstValue), new(RushOrder),
+		new(UsersLog))
+}
+
+
+func Connect() {
+	db_host := beego.AppConfig.String("database::db_host")
+	db_type := beego.AppConfig.String("database::db_type")
+	db_port := beego.AppConfig.String("database::db_port")
+	db_user := beego.AppConfig.String("database::db_user")
+	db_pass := beego.AppConfig.String("database::db_pass")
+	db_name := beego.AppConfig.String("database::db_name")
+
+	//db_sslmode := beego.AppConfig.String("db_sslmode")
+	fmt.Println("data_info:", db_type, db_name, db_host, db_port, db_user, db_pass)
+	var dns string
+
+	switch db_type {
+	case "mysql":
+		orm.RegisterDriver("mysql", orm.DRMySQL)
+		dns = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4", db_user, db_pass, db_host, db_port, db_name)
+		break
+	default:
+		beego.Critical("Database driver is not allowed.", db_type)
+		break
+	}
+
+	orm.RegisterDataBase("default", db_type, dns)
+}
+
+func InsertSuper() {
+	r := Role{Status: 2}
+	o.Read(&r, "Status")
+	//var role
+	//qs := o.QueryTable("role")
+	//qs.Filter("role__status", 2).Filter("role__name", "SuperAdmin").Values(&role)
+	fmt.Println("roles:", r.Id, r.Status, r.Name, r.Description)
+	user := new(User)
+	user.Active = 0
+	user.email = "123123@admin.com"
+	user.Pword = "123123"
+	user.Uid = "100011"
+	user.Role = &r
+
+	profile := new(Profile)
+	profile.UpdateTime = time.Now()
+	_, err := o.Insert(profile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	user.Profile = profile
+	user.CreateTime = time.Now()
+	_, err = o.Insert(user)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//m2m := o.QueryM2M(&user, "Role")
+	//n, err := m2m.Add(&r)
+	//if err != nil {
+	//	fmt.Println("failed..")
+	//}
+	//return n
+}
+
+func InsertRoles() {
+	fmt.Println("insert roles...")
+	role := [3]Role{
+		{Name: "User", Status: 0, Description: "normal user"},
+		{Name: "Admin", Status: 1, Description: "admin"},
+		{Name: "SuperAdmin", Status: 2, Description: "super user"},
+	}
+	for _, v := range role {
+		r := new(Role)
+		r.Name = v.Name
+		r.Status = v.Status
+		r.Description = v.Description
+		o.Insert(r)
+	}
+}
+
+func InserConst() {
+	fmt.Println("insert const value...")
+	cv := [2]ConstValue{
+		{ConstType: 1, ValueStr: "100067", UpdateTime: time.Now(), CreateTime: time.Now()},
+		{ConstType: 2, ValueStr: "100067", UpdateTime: time.Now(), CreateTime: time.Now()},
+	}
+	for _, v := range cv {
+		c := new(ConstValue)
+		c.CreateTime = v.CreateTime
+		c.UpdateTime = v.UpdateTime
+		c.ConstType = v.ConstType
+		c.ValueStr = v.ValueStr
+
+		o.Insert(c)
+	}
+}
+
+//func Createdb() {
+//	db_host := beego.AppConfig.String("db_host")
+//	db_type := beego.AppConfig.String("db_type")
+//	db_port := beego.AppConfig.String("db_port")
+//	db_user := beego.AppConfig.String("db_user")
+//	db_pass := beego.AppConfig.String("db_pass")
+//	db_name := beego.AppConfig.String("db_name")
+//
+//}
+
+func Syncdb() {
+	Connect()
+	o = orm.NewOrm()
+	name := "default"
+	force := true
+	verbose := true
+
+	err := orm.RunSyncdb(name, force, verbose)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	InsertRoles()
+	InsertSuper()
+	InserConst()
+
+	fmt.Println("Database init is complete.")
 }
